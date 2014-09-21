4916ad163232f7c86227f08b36684ebfe8480aa9
Flycoding <Flycoding@yeah.net>
2014-09-22 00:53:52 +0800

    another commit.
    Signed-off-by: Flycoding <Flycoding@yeah.net>

--- src/test/java/com/flyingh/demo/Demo.java
+++ src/test/java/com/flyingh/demo/Demo.java
@@ -1,5 +1,6 @@
 package com.flyingh.demo;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -9,6 +10,25 @@ import java.util.stream.Collectors;
 import org.junit.Test;
 
 public class Demo {
+
+	@Test
+	public void test4() {
+		final List<Integer> list = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
+		System.out.println(list);
+		list.subList(1, 3).subList(0, 1).clear();
+		System.out.println(list);
+	}
+
+	@Test
+	public void test3() {
+		final List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+		swap(list, 0, list.size() - 1);
+		System.out.println(list);
+	}
+
+	private void swap(List<Integer> list, int i, int j) {
+		list.set(i, list.set(j, list.get(i)));
+	}
 
 	@Test
 	public void test2() {
@@ -30,6 +50,7 @@ public class Demo {
 		Arrays.asList("a", "b", "a", "c", "d", "e", "d", "f", "b", "e").stream().collect(Collectors.toSet()).forEach(System.out::print);
 		System.out.println();
 		list.stream().map(Employee::getSalary).collect(Collectors.toCollection(TreeSet::new)).forEach(System.out::println);
+		list.stream().map(Employee::getName).collect(Collectors.toList()).forEach(System.out::println);
 	}
 
 }
